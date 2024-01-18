12cfd8f9cb38a7bd5ebe1bec46cf5389c36cd23d
DondonCo <dondon.co19@gmail.com>
2024-01-18 20:56:13 +0800

    first commit

---
+++ src/test/java/com/dondon/crudwithrest/CrudwithrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dondon.crudwithrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudwithrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
